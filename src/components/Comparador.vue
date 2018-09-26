6896428814473da84f16a300ef9547c8a648c0f7
Fernando Ortego <fernando.ortego.contractor@bbva.com>
2018-09-26 17:08:21 +0200

    vue router and axios added to project

---
+++ src/components/Comparador.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="products">
+        <ul>
+            <li class="item1"><router-link to="/">CINTAS DE CORRER</router-link> </li>
+            <li class="item2"><router-link to="">BICICLETAS ELÍPTICAS</router-link></li>
+            <li class="item3"><router-link to="">BICICLETAS ESTATICAS</router-link></li>
+            <li class="item4"><router-link to="">BIKING</router-link></li>
+            <li class="item5"><router-link to="">REMOS</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .products ul{
+        list-style: none;
+        top: 50%;
+        left: 50%;
+    }
+</style>
